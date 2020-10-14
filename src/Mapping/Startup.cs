5029c6c62d5ead9a650ca3402c2010513fc296a9
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-10-14 12:19:31 +0100

    Removed unneeded template code

--- src/Mapping/Startup.cs
+++ src/Mapping/Startup.cs
@@ -2,11 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Jpp.Common.Razor.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,7 +26,6 @@ namespace Mapping
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<GeocodeService>();
             services.AddSingleton<MapLayerService>();
             services.AddSingleton<EntityQueryService>();
